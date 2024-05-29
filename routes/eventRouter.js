2d2bc008aa5bf3284702876492dd1ae5c6fe39fd
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-05-29 17:42:53 +0200

    add isAuth in routes Event

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const { isAuth } = require('../middleware/isAuth')
+
 const {
   getEvents,
   getEvent
 } = require('../controllers/EventController/eventController')
 const ROUTER = express.Router()
 
-ROUTER.get('/', getEvents) // LIST EVENTS
-ROUTER.get('/:_id', getEvent) // EVENT
+ROUTER.get('/', isAuth, getEvents) // LIST EVENTS
+ROUTER.get('/:_id', isAuth, getEvent) // EVENT
 
 module.exports = ROUTER
