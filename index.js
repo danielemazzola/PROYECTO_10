432f573764c8de2f33b2b1659cff4f96cfb3d382
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-06-13 16:32:54 +0200

    FEAT process.env.PORT asigned to vercel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const CONNECTDB = require('./config/CONNECTDB')
 const CONNECTION_CLOUDINARY = require('./config/CONNECTION_CLOUDINARY')
 const APP = express()
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 APP.use(express.json())
 APP.use(express.urlencoded({ extended: false }))
